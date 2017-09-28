034bcae42db3cf07a3cfd157b0d02261136e332f
pad <p_ad@outlook.com>
2017-09-28 21:06:25 +0800

    init project

---
+++ src/main/java/com/pad/PadsiteApplication.java
@@ -0,0 +1,12 @@
+package com.pad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadsiteApplication.class, args);
+	}
+}
